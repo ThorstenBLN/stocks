a9f7cca780ec362c8658efc5caf7030abe4486fd
ThorstenBLN <tjostock@gmx.de>
2025-06-19 22:29:30 +0200

    debugging lstm 5

--- extract_lstm_data.py
+++ extract_lstm_data.py
@@ -159,5 +159,6 @@ if __name__ == "__main__":
     except Exception as err:
         logging.info(f"{dt.datetime.now().strftime('%d.%m.%Y %H:%M:%S')} Exception extract_lstm_data main")
         logging.error(err, stack_info=True, exc_info=True)
+        print(err)
         sys.exit(1)
 
