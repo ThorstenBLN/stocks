53d9c493eec7fa7a0f203760bf2d0fc6616b972f
ThorstenBLN <63974282+ThorstenBLN@users.noreply.github.com>
2025-07-09 17:26:50 +0200

    debugging manager wrong sales

--- depot_mgt.py
+++ depot_mgt.py
@@ -97,7 +97,7 @@ def main():
     df_sales = df_depot.loc[mask_1 | mask_2 | mask_3 | mask_4].copy().reset_index(drop=True)
     for row in df_sales.itertuples():
         # if all purchase options are already in depot -> break  // exlcude there is none as this means we hold even bad stocks, cause there is no alternative
-        if df_pur_opt['in_dpt'].unique().mean() == 1: # len(df_pur_opt['in_dpt'].unique()) == 0 or
+        if not df_pur_opt.empty and df_pur_opt['in_dpt'].unique().mean() == 1: # len(df_pur_opt['in_dpt'].unique()) == 0 or
             break
         # do not sell a stock you would directly rebuy (because it is the best option   ):
         if row.isin in df_pur_opt['isin'].unique():
