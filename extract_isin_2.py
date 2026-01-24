fd431b421f1b215c05659eb0583c1c990b35de07
ThorstenBLN <63974282+ThorstenBLN@users.noreply.github.com>
2026-01-24 11:32:19 +0100

    debug isin 3

--- extract_isin_2.py
+++ extract_isin_2.py
@@ -65,6 +65,7 @@ def main():
         df_extra = df_xetra.merge(df_used[['isin', 'state']], on='isin', how='left')
         print(1.1)
         df_used.drop(columns=['state', 'exclude_lstm'], inplace=True)
+        print(1.1.1)
         df_extra['state'] = np.where((df_xetra['state'].isna()), 2, df_extra['state']) # 2 = new
         print(1.2)
         # 2. define valid exisiting isin to recheck
