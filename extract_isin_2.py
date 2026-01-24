96e49f0ba76f0075d0976d983d31aea7b7a7c62b
ThorstenBLN <63974282+ThorstenBLN@users.noreply.github.com>
2026-01-24 12:08:46 +0100

    degbug isin 7

--- extract_isin_2.py
+++ extract_isin_2.py
@@ -62,7 +62,6 @@ def main():
     if df_used is not None:
         # 1. define current xetra symbols to delete if not in xetra file anymore
         df_used['state'] = 1 # 1 = valid
-        print(df_extra.columns)
         df_extra = df_xetra.merge(df_used[['isin', 'state']], on='isin', how='left')
         print(df_extra.columns)
         print(1.1)
